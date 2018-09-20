00f74cd329a3168871eeeea8b9592dadaff57f54
Simon Aroca <web.simonaroca@gmail.com>
2018-09-20 10:53:56 +0200

    Vue index statique

---
+++ burstfly/resources/views/base.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title')
+  Laravel - Burstfly
+@endsection
+
+@section('link')
+  <link rel="icon" type="image/png" href="{{asset('img/small-logo-01.png')}}">
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,900,900italic,700italic,700,500italic,400italic,500,300italic,300' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+  <link href='{{asset('css/style.css')}}' rel='stylesheet' type='text/css'>
+@endsection
